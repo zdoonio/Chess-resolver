22bd15bcdfc3dbac94431662992eb79c369ecb12
Dominik Zduńczyk <dominik.zdunczyk@gmail.com>
2019-01-25 00:47:22 +0100

    Added more resolver tests.

--- src/test/scala/ResolverTest.scala
+++ src/test/scala/ResolverTest.scala
@@ -9,13 +9,35 @@ import resolver.Resolver
   */
 class ResolverTest extends FlatSpec with Matchers {
 
-  "Rook" should "get all possible permutations for board 2x2 with two Rooks" in {
+  "Resolver" should "get all possible permutations for board 2x2 with two Rooks" in {
 
     val board = ChessBoard(2,2)
     val rook = ChessPiece(Chessman.ROOK)
     val game = Game(board,List(rook, rook))
 
     Resolver.solve(game).size should be (2)
+
+  }
+
+  "Resolver" should "get all possible permutations for board 3x3 with two Kings and one Rook" in {
+
+    val board = ChessBoard(3,3)
+    val rook = ChessPiece(Chessman.ROOK)
+    val king = ChessPiece(Chessman.KING)
+    val game = Game(board,List(rook, king, king))
+
+    Resolver.solve(game).size should be (4)
+
+  }
+
+  "Resolver" should "get all possible permutations for board 4x4 with two Rooks and four Knights" in {
+
+    val board = ChessBoard(4,4)
+    val rook = ChessPiece(Chessman.ROOK)
+    val knight = ChessPiece(Chessman.KNIGHT)
+    val game = Game(board,List(rook, rook, knight, knight, knight, knight))
+
+    Resolver.solve(game).size should be (8)
 
   }
 
